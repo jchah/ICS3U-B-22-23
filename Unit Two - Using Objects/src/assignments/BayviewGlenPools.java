9615400a06c4ed49160e7dc7526c63e15e96f274
Jad <98987031+jchah@users.noreply.github.com>
2022-10-18 15:10:20 -0400

    comments + calculation corrections

--- Unit Two - Using Objects/src/assignments/BayviewGlenPools.java
+++ Unit Two - Using Objects/src/assignments/BayviewGlenPools.java
@@ -3,9 +3,9 @@ package assignments;
 import java.util.Scanner;
 
 public class BayviewGlenPools {
-    // TODO: change variable names
     static Scanner i = new Scanner(System.in);
 
+    // method to simplify input
     static int inquire(String q) {
         System.out.printf("Input %s: ", q);
 
@@ -13,20 +13,21 @@ public class BayviewGlenPools {
     }
 
     public static void main(String[] args) {
+        // prompts user for input
          int l = inquire("length"), w = inquire("width"), ds = inquire("shallow depth"),
              dd = inquire("deep depth"), t = inquire("transition slope"), ls = inquire("shallow length"),
-             p = inquire("price of liner"), tHeight = dd - ds;
+             p = inquire("price of liner/m²"), tHeight = dd - ds;
 
+        // calculations for volume, surface area, and liner cost
         double tLength = Math.sqrt(t*t-tHeight*tHeight),
                 ld = l - ls - tLength,
                 volume = w * ds * ls + w * dd * ld + tLength * ds * w + 0.5 * w * tLength * tHeight,
-                surfaceArea = w * dd + w * ds + 2 * (l * ds + (ld + ld + tLength) * tHeight / 2) + (ld + ls) * w + t,
+                surfaceArea = ds * ls * 2 + ls * w + (ds * tLength + tLength * (dd - ds) / 2) * 2 +
+                        t * w + dd * ld * 2 + ld * w + w * dd + w * ds,
                 cost = surfaceArea * p;
 
         System.out.printf("Water for 90%%: %.2fL\n", volume*900);
         System.out.printf("Liner needed: %.2f cm²\n", surfaceArea);
         System.out.printf("Liner cost @ $%d/cm²: $%.2f", p, cost);
-
-
     }
 }
