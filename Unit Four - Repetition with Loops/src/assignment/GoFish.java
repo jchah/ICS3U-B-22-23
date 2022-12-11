accec70c1e6f8ef000944d7a3e5e57d7cd55a1c4
jchahin1121 <jchahin11@gmail.com>
2022-12-11 17:44:55 -0500

    - added player turn - added playing loop - replaced inefficient ifs with switch

--- Unit Four - Repetition with Loops/src/assignment/GoFish.java
+++ Unit Four - Repetition with Loops/src/assignment/GoFish.java
@@ -10,35 +10,74 @@ public class GoFish {
 
     public static void main(String[] args) {
         String cards1, cards2, cards3, cards4;
-        int score1, score2, score3, score4;
+        int score1 = 0, score2 = 0, score3 = 0, score4 = 0;
         String temp;
+
         cards1 = getHand();
         cards2 = getHand();
         cards3 = getHand();
         cards4 = getHand();
-        System.out.println("ORIGINAL DRAW: " + cards1);
-        temp = checkPairs(cards1);
-        cards1 = parseHand(temp);
-        score1 = parseScore(temp);
-        temp = checkPairs(cards2);
-        cards2 = parseHand(temp);
-        score2 = parseScore(temp);
-        temp = checkPairs(cards3);
-        cards3 = parseHand(temp);
-        score3 = parseScore(temp);
-        temp = checkPairs(cards4);
-        cards4 = parseHand(temp);
-        score4 = parseScore(temp);
-        System.out.println("PAIR ADJUSTED: " + cards1);
-        System.out.println("PLAYER SCORE: " + score1);
-        System.out.println("COMP1 SCORE: " + score2);
-        System.out.println("COMP2 SCORE: " + score3);
-        System.out.println("COMP3 SCORE: " + score4);
 
-        /* Prompt what card to ask for (must be a card already in hand) and which player to ask.
-        If the computer has the card, take the card, add to score, and remove the pair from the
-        deck. Otherwise, the computer says go fish, and you take another card. Every bot must do
-        this, implement random functionality to ask random players for the computers turn. */
+        System.out.println("ORIGINAL DRAW: " + cards1);
+
+        while (score1 < 10 && score2 < 10 && score3 < 10 && score4 < 10) {
+            temp = checkPairs(cards1);
+            cards1 = parseHand(temp);
+            score1 += parseScore(temp);
+            temp = checkPairs(cards2);
+            cards2 = parseHand(temp);
+            score2 += parseScore(temp);
+            temp = checkPairs(cards3);
+            cards3 = parseHand(temp);
+            score3 += parseScore(temp);
+            temp = checkPairs(cards4);
+            cards4 = parseHand(temp);
+            score4 += parseScore(temp);
+
+            System.out.println("PAIR ADJUSTED HAND: " + cards1);
+            System.out.println("PLAYER SCORE: " + score1);
+            System.out.println("COMP1 SCORE: " + score2);
+            System.out.println("COMP2 SCORE: " + score3);
+            System.out.println("COMP3 SCORE: " + score4);
+
+            System.out.print("Card request: ");
+            String face = in.nextLine();
+            System.out.print("From player: ");
+            int player = in.nextInt();
+            String suit;
+            String card;
+
+            /* Prompt what card to ask for (must be a card already in hand) and which player to ask.
+            If the computer has the card, take the card, add to score, and remove the pair from the
+            deck. Otherwise, the computer says go fish, and you take another card. Every bot must do
+            this, implement random functionality to ask random players for the computers turn. */
+
+            // TODO: Implement bot play
+
+            if (player == 2 && cards2.contains(face)) {
+                suit = cards2.charAt(cards2.indexOf(face) + 1) + "";
+                card = face + suit;
+                cards1 += card;
+                cards2 = cards2.replaceFirst(card, "");
+                System.out.printf("You got %s from player %d", card, player);
+            } else if (player == 3 && cards3.contains(face)) {
+                suit = cards3.charAt(cards3.indexOf(face) + 1) + "";
+                card = face + suit;
+                cards1 += card;
+                cards3 = cards3.replaceFirst(card, "");
+                System.out.printf("You got %s from player %d", card, player);
+            } else if (player == 4 && cards4.contains(face)) {
+                suit = cards4.charAt(cards4.indexOf(face) + 1) + "";
+                card = face + suit;
+                cards1 += card;
+                cards4 = cards4.replaceFirst(card, "");
+                System.out.printf("You got %s from player %d", card, player);
+            } else {
+                System.out.printf("Player %d says \"Go fish!\"", player);
+                card = getCard();
+                System.out.printf("You randomly draw %s", card);
+            }
+        }
     }
 
     private static int parseScore(String hand) {
@@ -52,37 +91,35 @@ public class GoFish {
     private static String getHand() {
         StringBuilder cards = new StringBuilder();
         for (int i=0; i< 5; i++) {
-            cards.append(getFace()).append(getSuit());
+            cards.append(getCard());
         }
         return cards.toString();
     }
 
+    private static String getCard() {
+        return getFace() + getSuit();
+    }
+
     private static String getFace() {
         int rng = (int) (Math.random() * NUM_FACE) + 1;
-        if(rng == 1)
-            return "A";
-        else if (rng == 10)
-            return "X";
-        else if (rng == 11)
-            return "J";
-        else if (rng == 12)
-            return "Q";
-        else if (rng == 13)
-            return "K";
-        else
-            return rng + "";
+        return switch (rng) {
+            case 1 -> "A";
+            case 10 -> "X";
+            case 11 -> "J";
+            case 12 -> "Q";
+            case 13 -> "K";
+            default -> rng + "";
+        };
     }
 
     private static String getSuit() {
         int rng = (int) (Math.random() * NUM_SUITS) + 1;
-        if(rng == 1)
-            return "H";
-        else if (rng == 2)
-            return "C";
-        else if (rng == 3)
-            return "S";
-        else
-            return "D";
+        return switch (rng) {
+            case 1 -> "H";
+            case 2 -> "C";
+            case 3 -> "S";
+            default -> "D";
+        };
     }
 
     private static String checkPairs(String hand) {
