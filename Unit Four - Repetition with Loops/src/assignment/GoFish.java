95c9a372b2c9f72892d27309420716609e741b22
Jad <98987031+jchah@users.noreply.github.com>
2022-12-18 23:04:35 -0500

    - Fixed possibility of card in a pair being given away instead of put down - Players now get new hand once they run out (work in progress)

--- Unit Four - Repetition with Loops/src/assignment/GoFish.java
+++ Unit Four - Repetition with Loops/src/assignment/GoFish.java
@@ -1,5 +1,4 @@
 package assignment;
-
 import java.util.Scanner;
 
 public class GoFish {
@@ -7,78 +6,160 @@ public class GoFish {
     private static final int NUM_SUITS = 4;
     private static final int RESET_CARDS = -2;
     static final Scanner in  = new Scanner(System.in);
-
     public static void main(String[] args) {
         String cards1, cards2, cards3, cards4;
         int score1 = 0, score2 = 0, score3 = 0, score4 = 0;
         String temp;
+        String suit;
+        String card;
+        String hand;
+        int beginIndex = 1; // 1 % 2 != 0
+        int randPlayer = (int) (Math.random() * 4) + 1;
+        String randCard;
 
         cards1 = getHand();
         cards2 = getHand();
         cards3 = getHand();
         cards4 = getHand();
 
-        System.out.println("ORIGINAL DRAW: " + cards1);
+        System.out.println("Original draw: " + cards1);
+
+        temp = checkPairs(cards1);
+        cards1 = parseHand(temp);
+        score1 += parseScore(temp);
+        temp = checkPairs(cards2);
+        cards2 = parseHand(temp);
+        score2 += parseScore(temp);
+        temp = checkPairs(cards3);
+        cards3 = parseHand(temp);
+        score3 += parseScore(temp);
+        temp = checkPairs(cards4);
+        cards4 = parseHand(temp);
+        score4 += parseScore(temp);
 
         while (score1 < 10 && score2 < 10 && score3 < 10 && score4 < 10) {
-            temp = checkPairs(cards1);
-            cards1 = parseHand(temp);
-            score1 += parseScore(temp);
-            temp = checkPairs(cards2);
-            cards2 = parseHand(temp);
-            score2 += parseScore(temp);
-            temp = checkPairs(cards3);
-            cards3 = parseHand(temp);
-            score3 += parseScore(temp);
-            temp = checkPairs(cards4);
-            cards4 = parseHand(temp);
-            score4 += parseScore(temp);
+            System.out.println("Pair adjusted hand: " + cards1);
+            System.out.println("Player 1 (you) score: " + score1);
+            System.out.println("Player 2 score: " + score2);
+            System.out.println("Player 3 score: " + score3);
+            System.out.println("Player 4 score: " + score4);
 
-            System.out.println("PAIR ADJUSTED HAND: " + cards1);
-            System.out.println("PLAYER SCORE: " + score1);
-            System.out.println("COMP1 SCORE: " + score2);
-            System.out.println("COMP2 SCORE: " + score3);
-            System.out.println("COMP3 SCORE: " + score4);
+            System.out.println("cards2: " + cards2);
+            System.out.println("cards3: " + cards3);
+            System.out.println("cards4: " + cards4);
 
             System.out.print("Card request: ");
             String face = in.nextLine();
             System.out.print("From player: ");
             int player = in.nextInt();
-            String suit;
-            String card;
-
-            /* Prompt what card to ask for (must be a card already in hand) and which player to ask.
-            If the computer has the card, take the card, add to score, and remove the pair from the
-            deck. Otherwise, the computer says go fish, and you take another card. Every bot must do
-            this, implement random functionality to ask random players for the computers turn. */
-
-            // TODO: Implement bot play
+            in.nextLine(); // clears buffer
 
             if (player == 2 && cards2.contains(face)) {
                 suit = cards2.charAt(cards2.indexOf(face) + 1) + "";
                 card = face + suit;
                 cards1 += card;
                 cards2 = cards2.replaceFirst(card, "");
-                System.out.printf("You got %s from player %d", card, player);
+                System.out.printf("You got %s from player %d\n", card, player);
             } else if (player == 3 && cards3.contains(face)) {
                 suit = cards3.charAt(cards3.indexOf(face) + 1) + "";
                 card = face + suit;
                 cards1 += card;
                 cards3 = cards3.replaceFirst(card, "");
-                System.out.printf("You got %s from player %d", card, player);
+                System.out.printf("You got %s from player %d\n", card, player);
             } else if (player == 4 && cards4.contains(face)) {
                 suit = cards4.charAt(cards4.indexOf(face) + 1) + "";
                 card = face + suit;
                 cards1 += card;
                 cards4 = cards4.replaceFirst(card, "");
-                System.out.printf("You got %s from player %d", card, player);
+                System.out.printf("You got %s from player %d\n", card, player);
             } else {
                 card = getCard();
                 cards1 += card;
-                System.out.printf("Player %d says \"Go fish!\" You randomly draw %s\n", player, card);
-                System.out.println(cards1);
+                System.out.printf("Player %d says \"go fish!\" and you randomly draw %s\n", player, card);
+            }
+
+            temp = checkPairs(cards1);
+            cards1 = parseHand(temp);
+            score1 += parseScore(temp);
+
+            for(int i = 2; i < 5; i++) {
+                //FIXME On last iteration hands with one card will not be fixed if it's given away
+                cards1 = fixHand(cards1);
+                cards2 = fixHand(cards2);
+                cards3 = fixHand(cards3);
+                cards4 = fixHand(cards4);
+                while (randPlayer == i) {
+                    randPlayer = (int) (Math.random() * 4) + 1;
+                }
+                if (i == 2)
+                    hand = cards2;
+                else if (i == 3)
+                    hand = cards3;
+                else
+                    hand = cards4;
+
+                while (beginIndex % 2 != 0) {
+                    beginIndex = (int) (Math.random() * hand.length()) - 1;
+                }
+                randCard = hand.substring(beginIndex, beginIndex + 2);
+                face = String.valueOf(randCard.charAt(0));
+                System.out.printf("Player %d asks player %d for %s\n", i, randPlayer, face);
+
+                if (randPlayer == 1 && cards1.contains(face)) {
+                    suit = cards1.charAt(cards1.indexOf(face) + 1) + "";
+                    card = face + suit;
+                    hand += card;
+                    cards1 = cards1.replaceFirst(card, "");
+                    System.out.printf("Player %d gives %s to player %d\n", randPlayer, card, i);
+                } else if (randPlayer == 2 && cards2.contains(face)) {
+                    suit = cards2.charAt(cards2.indexOf(face) + 1) + "";
+                    card = face + suit;
+                    hand += card;
+                    cards2 = cards2.replaceFirst(card, "");
+                    System.out.printf("Player %d gives %s to player %d\n", randPlayer, card, i);
+                } else if (randPlayer == 3 && cards3.contains(face)) {
+                    suit = cards3.charAt(cards3.indexOf(face) + 1) + "";
+                    card = face + suit;
+                    hand += card;
+                    cards3 = cards3.replaceFirst(card, "");
+                    System.out.printf("Player %d gives %s to player %d\n", randPlayer, card, i);
+                } else if (randPlayer == 4 && cards4.contains(face)) {
+                    suit = cards4.charAt(cards4.indexOf(face) + 1) + "";
+                    card = face + suit;
+                    hand += card;
+                    cards4 = cards4.replaceFirst(card, "");
+                    System.out.printf("Player %d gives %s to player %d\n", randPlayer, card, i);
+                } else {
+                    card = getCard();
+                    hand += card;
+                    System.out.printf("Player %d says \"go fish!\" and player %d randomly draws %s\n", randPlayer, i, card);
+                }
+
+                temp = checkPairs(hand);
+                hand = parseHand(temp);
+                if (i == 2) {
+                    score2 += parseScore(temp);
+                    cards2 = hand;
+                }
+                else if (i == 3) {
+                    score3 += parseScore(temp);
+                    cards3 = hand;
+                } else {
+                    score4 += parseScore(temp);
+                    cards4 = hand;
+                }
+                beginIndex = 1;
             }
         }
+    }
+
+    private static String fixHand(String hand) {
+        if (hand.length() > 0) {
+            return hand;
+        }
+        String temp = getHand();
+        System.out.println("Gave new hand: " + temp);
+        return temp;
     }
 
     private static int parseScore(String hand) {
@@ -91,9 +172,8 @@ public class GoFish {
 
     private static String getHand() {
         StringBuilder cards = new StringBuilder();
-        for (int i=0; i< 5; i++) {
+        for (int i=0; i< 5; i++)
             cards.append(getCard());
-        }
         return cards.toString();
     }
 
