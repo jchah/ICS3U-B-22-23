270fb12d0f1775305d77ed71198cee8d31979d5e
Jad <98987031+jchah@users.noreply.github.com>
2023-02-05 22:00:04 -0500

    removed unused import statement

--- Unit Six - Using Arrays/src/DoubleArraySequence.java
+++ Unit Six - Using Arrays/src/DoubleArraySequence.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 /******************************************************************************
  * A DoubleArraySequence is a collection of double numbers.
  * The sequence can have a special "current element," which is specified and
